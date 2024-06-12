1558e7ac7949f18dcaf992cb923d0d8011014d5a
Adameia Ramsey <adameiaproductions@gmail.com>
2024-06-12 10:35:29 -0500

    ran into issue with item soft delete that may require database or model changes

--- Controllers/ItemController.cs
+++ Controllers/ItemController.cs
@@ -29,7 +29,68 @@ public class ItemController : ControllerBase
     public IActionResult GetByUser(int id)
     {
         return Ok(_dbContext.Items
-        .Where(i => i.ItemId == id)
+        .Where(i => i.UserId == id)
         .ToList());
+    }
+
+    [HttpPost]
+    //[Authorize]
+    public IActionResult CreateItem(Item item)
+    {
+        _dbContext.Items.Add(item);
+        _dbContext.SaveChanges();
+        return Created($"api/items/{item.ItemId}", item);
+    }
+
+    [HttpPut("{id}")]
+    //[Authorize]
+    public IActionResult EditItem(Item item, int id)
+    {
+        Item itemToUpdate = _dbContext.Items.SingleOrDefault(i => i.ItemId == id);
+        if (itemToUpdate == null)
+        {
+            return NotFound();
+        }
+        else if (id != itemToUpdate.ItemId)
+        {
+            return BadRequest();
+        }
+
+        itemToUpdate.Description = item.Description;
+        itemToUpdate.FloorId = item.FloorId;
+
+        _dbContext.SaveChanges();
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    //[Authorize]
+    public IActionResult ToggleItem(int id, int? newFloorId)
+    // ok. PSQL/EFC is too smart to let me reassign this to a nonexistent floor. I will have to nuke the database and start over, adding a key to Items for activity. Or I could just do a simpler delete? Decisions.
+    {
+        Item itemToToggle = _dbContext.Items.SingleOrDefault(i => i.ItemId == id);
+        if (itemToToggle.ItemId == null)
+        {
+            return NotFound();
+        }
+        else if (id != itemToToggle.ItemId)
+        {
+            return BadRequest();
+        }
+
+        if (itemToToggle.FloorId != 0)
+        {
+            itemToToggle.FloorId = 0;
+            _dbContext.SaveChanges();
+            return NoContent();
+        }
+        else if (newFloorId != null)
+        {
+            itemToToggle.FloorId = (int)newFloorId;
+            _dbContext.SaveChanges();
+            return NoContent();
+        }
+
+        return BadRequest("No new floorId entered");
     }
 }
